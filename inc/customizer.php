612a78a468f534a2d412fdc55ddc03fae2578b9f
saikat mondal <62213201+codingbysaikat@users.noreply.github.com>
2022-10-04 20:41:47 +0600

    up to date

--- inc/customizer.php
+++ inc/customizer.php
@@ -7,7 +7,7 @@ function resto_customize_register($wp_customizer){
         'priority' => 30, 
     ));
     $wp_customizer->add_setting( 'resto_cus_home_sett', array(
-        'default' => 'We Love Delicious Foods!',
+        'default' => __('We Love Delicious Foods!','resto'),
         'transport' => 'refresh', // or postMessage
         
       ) );
@@ -16,7 +16,19 @@ function resto_customize_register($wp_customizer){
         'section' => 'resto_cus_home_sec',
         'settings'=>'resto_cus_home_sett',        
         'label' => __( 'Theme title' ),
-        'description' => __( 'This is the range control description.' ),
+        'description' => __( 'This is the range control description.','resto' ),
       ) );
+      $wp_customizer->add_setting( 'resto_support_time_sett', array(
+        'default' =>7,
+        'transport' => 'refresh', // or postMessage
+        
+      ) );
+      $wp_customizer->add_control('resto_support_time_ctrl',array(
+        'type'=>'number',
+        'section' =>'resto_cus_home_sec',
+        'settings'=>'resto_support_time_sett',
+        'label' => __(' Your Shop Support time'),
+        'description'=> __('set your shop support time')
+      ));
 }
 add_action('customize_register', 'resto_customize_register');
